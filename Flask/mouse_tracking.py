327866aad532e73f8d16db758b8ed43b3435e4ba
TessaVogt <tvogt@muenster.de>
2023-07-27 17:26:00 +0200

    Update Flask

--- Flask/mouse_tracking.py
+++ Flask/mouse_tracking.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import sqlite3
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coords.db'
